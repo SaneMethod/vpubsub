f413559545887c7b15691e99d5117d4241f69bc4
Christopher Keefer <sanemethodology@gmail.com>
2026-07-21 16:22:05 -0400

    Dependencies update.

--- dist/vpubsub.full.js
+++ dist/vpubsub.full.js
@@ -125,82 +125,9 @@ function isObjectLike(value) {
   return value != null && typeof value == 'object';
 }
 
-/** `Object#toString` result references. */
-var symbolTag = '[object Symbol]';
-
-/**
- * Checks if `value` is classified as a `Symbol` primitive or object.
- *
- * @static
- * @memberOf _
- * @since 4.0.0
- * @category Lang
- * @param {*} value The value to check.
- * @returns {boolean} Returns `true` if `value` is a symbol, else `false`.
- * @example
- *
- * _.isSymbol(Symbol.iterator);
- * // => true
- *
- * _.isSymbol('abc');
- * // => false
- */
-function isSymbol(value) {
-  return typeof value == 'symbol' ||
-    (isObjectLike(value) && baseGetTag(value) == symbolTag);
-}
-
-/**
- * A specialized version of `_.map` for arrays without support for iteratee
- * shorthands.
- *
- * @private
- * @param {Array} [array] The array to iterate over.
- * @param {Function} iteratee The function invoked per iteration.
- * @returns {Array} Returns the new mapped array.
- */
-function arrayMap(array, iteratee) {
-  var index = -1,
-      length = array == null ? 0 : array.length,
-      result = Array(length);
-
-  while (++index < length) {
-    result[index] = iteratee(array[index], index, array);
-  }
-  return result;
-}
-
-/**
- * Checks if `value` is classified as an `Array` object.
- *
- * @static
- * @memberOf _
- * @since 0.1.0
- * @category Lang
- * @param {*} value The value to check.
- * @returns {boolean} Returns `true` if `value` is an array, else `false`.
- * @example
- *
- * _.isArray([1, 2, 3]);
- * // => true
- *
- * _.isArray(document.body.children);
- * // => false
- *
- * _.isArray('abc');
- * // => false
- *
- * _.isArray(_.noop);
- * // => false
- */
-var isArray = Array.isArray;
-
-/** Used as references for various `Number` constants. */
-var INFINITY = 1 / 0;
-
 /** Used to convert symbols to primitives and strings. */
-var symbolProto = Symbol ? Symbol.prototype : undefined,
-    symbolToString = symbolProto ? symbolProto.toString : undefined;
+var symbolProto = Symbol ? Symbol.prototype : undefined;
+    symbolProto ? symbolProto.toString : undefined;
 
 /**
  * The base implementation of `_.toString` which doesn't convert nullish
@@ -212,18 +139,9 @@ var symbolProto = Symbol ? Symbol.prototype : undefined,
  */
 function baseToString(value) {
   // Exit early for strings to avoid a performance hit in some environments.
-  if (typeof value == 'string') {
+  {
     return value;
   }
-  if (isArray(value)) {
-    // Recursively convert values (susceptible to call stack limits).
-    return arrayMap(value, baseToString) + '';
-  }
-  if (isSymbol(value)) {
-    return symbolToString ? symbolToString.call(value) : '';
-  }
-  var result = (value + '');
-  return (result == '0' && (1 / value) == -INFINITY) ? '-0' : result;
 }
 
 /**
@@ -325,7 +243,7 @@ function overArg(func, transform) {
  * // => '1,2,3'
  */
 function toString(value) {
-  return value == null ? '' : baseToString(value);
+  return baseToString(value);
 }
 
 /** Built-in value references. */
@@ -464,10 +382,10 @@ class Vent{
 
     /**
      * Trigger the given event on the given channel, passing any additional arguments to the event handler.
-     * Note that you can subscribe on a single event ('event' - with the implicit default channel, or 'event.channel');
-     * or you can subscribe on all events for a channel ('*.channel', with '*' getting the implicit default channel),
-     * or you can subscribe to all events ('*.*'). You can not, however, subscribe for a certain event on all channels
-     * (e.g. 'event.*') - doing so will just subscribe you to the 'event' on the '*' channel, which is the same
+     * Note that you can subscribe on a single event ('event' - with the implicit default channel, or 'channel.event');
+     * or you can subscribe on all events for a channel ('channel.*', with '*' getting the implicit default channel),
+     * or you can subscribe to all events ('*.*'). You cannot subscribe for a certain event on all channels
+     * (e.g. '*.event') - doing so will just subscribe you to the 'event' on a '*' channel, which is the same
      * as registering for a normal event on any channel.
      * @param {string} channelEvent
      * @param {...any} args
